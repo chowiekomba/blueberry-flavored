4d21cd39d7c15b09431780a94b218a3955d092d2
chowiekomba <chowiekomba@gmail.com>
2026-08-12 18:30:31 -0600

    1.5x durability and +1 speed on tools

---
+++ src/main/java/chowie/blueberryflavored/mixin/ToolMaterialMixin.java
@@ -0,0 +1,27 @@
+package chowie.blueberryflavored.mixin;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.ToolMaterial;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ToolMaterial.class)
+public class ToolMaterialMixin {
+    @SuppressWarnings("rawtypes")
+    @Definition(id = "ToolMaterial", type = ToolMaterial.class)
+    @Expression("new ToolMaterial(?, ?, ?, ?, ?, ?)")
+    @WrapOperation(
+            method = "<clinit>",
+            at = @At(value = "MIXINEXTRAS:EXPRESSION")
+    )
+    private static ToolMaterial inToolConstructors(
+            TagKey incorrectBlocksForDrops, int durability, float speed, float attackDamageBonus, int enchantmentValue,
+            TagKey repairItems, Operation<ToolMaterial> original) {
+        return original.call(incorrectBlocksForDrops, (int) (durability * 1.5), speed + 1, attackDamageBonus,
+                enchantmentValue, repairItems);
+    }
+}
